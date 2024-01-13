542a40902ff21aa8abe3c0908655373c09a3bfb8
suresh-89 <87361260+suresh-89@users.noreply.github.com>
2024-01-13 09:00:57 +0530

    Add files via upload

---
+++ Inquel-Front/src/shared/baseUrl.js
@@ -0,0 +1,18 @@
+//export const baseUrl = "https://api.iqlabsacademy.com/";
+//export const baseUrl = "https://app.iqlabsacademy.com/";
+export const baseUrl = "http://127.0.0.1:8000/"
+
+export const accountsUrl = "2d7a_3fde/api/v1/accounts";
+
+export const adminPathUrl = "52fd_1f4a/api/v1/admin";
+
+export const inquelAdminUrl = "22de_06a2/api/v1/inquel_admin";
+
+export const hodUrl = "424f_0659/api/v1";
+
+export const teacherUrl = "3a3c_a1a6/api/v1";
+
+export const studentUrl = "5ddf_cae2/api/v1";
+
+export const homeURL = "a69e_f43e/api/v1/website";
+    
---
+++ Inquel-Front/src/shared/constant.js
@@ -0,0 +1,21 @@
+export const paginationCount = 10;
+
+// THUMBNAIL IMAGES
+export const GROUP_THUMBNAIL =
+    "https://iqlabs-media-type1.s3.us-east-2.amazonaws.com/media/app_data/second.jpg";
+export const SUBJECT_THUMBNAIL =
+    "https://iqlabs-media-type1.s3.us-east-2.amazonaws.com/media/app_data/first.jpg";
+
+// QUIZ AUDIO
+export const QUIZ_AUDIO =
+    "https://iqlabs-media-type1.s3.us-east-2.amazonaws.com/media/app_data/background-music.mp3";
+
+// TEMPLATE URL
+export const MATCH_TEMPLATE =
+    "https://iqlabs-media-type1.s3.us-east-2.amazonaws.com/media/Match/Templates/TeacherMatchTemplate.xlsx";
+export const CONCEPT_TEMPLATE =
+    "https://iqlabs-media-type1.s3.us-east-2.amazonaws.com/media/TypeOne/Templates/TeacherConceptsTemplate.xlsx";
+export const TYPE_ONE_TEMPLATE =
+    "https://iqlabs-media-type1.s3.us-east-2.amazonaws.com/media/TypeOne/Templates/TeacherTypeOneTemplate.xlsx";
+export const TYPE_TWO_TEMPLATE =
+    "https://iqlabs-media-type1.s3.us-east-2.amazonaws.com/media/TypeTwo/Templates/TeacherTypeTwoTemplate.xlsx";
